8b60ff95133d7aeeb9ced5126bb7739ab0098d6b
samrath-sudesh-acharya <samrathsacharya@gmail.com>
2021-06-18 10:06:38 +0530

    Adding setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get install macchanger
+
+echo "$(tput setaf 4)*****************************************$(tput sgr 0)"
+echo "$(tput setaf 4)*                                       *$(tput sgr 0)"
+echo "$(tput setaf 4)* Mac_changer is successfully installed *$(tput sgr 0)"
+echo "$(tput setaf 4)*                                       *$(tput sgr 0)"
+echo "$(tput setaf 4)* Please use : python mac_changer -h    *$(tput sgr 0)"
+echo "$(tput setaf 4)*                                       *$(tput sgr 0)"
+echo "$(tput setaf 4)*****************************************$(tput sgr 0)"
+
